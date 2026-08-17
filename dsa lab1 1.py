f6fe06af79ccf756242323ced2097515c30f84fb
pasalapreetham08-afk <pasalapreetham08@gmail.com>
2026-08-17 23:01:04 +0530

    Add files via upload

---
+++ dsa lab1 1.py
@@ -0,0 +1,10 @@
+def launch(n):
+    if n>=1:
+        print(n)
+        launch(n-1)
+
+launch(10)
+
+
+print('launch completed')
+ 
---
+++ dsa lab3 3.py
@@ -0,0 +1,29 @@
+# SELECTION SORT
+
+def selectionSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+# INPUT FROM USER
+
+arr = []
+n = int(input("Enter size of the list: "))
+
+print("Enter the elements:")
+
+for i in range(n):
+    arr.append(int(input()))
+
+print("Sorted array:")
+print(selectionSort(arr))
